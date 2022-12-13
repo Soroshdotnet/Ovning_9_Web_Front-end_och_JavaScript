314887a5373dd797b98bc396d128b2e8686d139a
soroshdotnet <sorosh.farahmand@gmail.com>
2022-12-13 13:06:10 +0100

    Lyckas ej hämta input och lägga i rätt variabel

--- bishbosh.js
+++ bishbosh.js
@@ -1,3 +1,6 @@
+
+
+
 // Bish Bosh Bish-Bosh
 
 // showMessage("HEEEEEEEJJJ");
@@ -5,95 +8,118 @@
 // const p = document.querySelector('#test')
 // document.getElementById("test").innerHTML
 
+const form = document.querySelector('#divisionForm')
+const output = document.querySelector('#output')
+
+
+//---------- Variabler -----------
 
 let bish = 'Bish';
 let bosh = 'Bosh';
 let bibo = 'Bish-Bosh';
 
-//-- bisBish function få upp den till card
+let loopTalVar = 100;
+let divisionsTal1Var = 3;
+let divisionsTal2Var = 4;
 
-
-document.getElementById('test');
-// document.getElementById('test2').innerHTML;
-for (i = 0; i <= 100; i++) {
-    if (i % 3 == 0)
-        document.writeln(bish);
-    else if (i % 4 == 0)
-        document.writeln(bosh);
-    else if (i % 3 == 0 && i % 4 == 0)
-        document.writeln(bibo);
-    document.writeln(i);
+function showMessage(message) {
+    document.getElementById('test').innerHTML += message;
+    /* document.getElementById('test2').innerHTML = message; */
 }
 
-
-
-
-
+// --------  BISHBOSH ON CARD --------
 function bishBosh() {
 
-    // kod här
+    for (i = 0; i <= loopTalVar; i++) {
+        if (i % divisionsTal1Var == 0)
+            showMessage(bish + ' ');
+        else if (i % divisionsTal2Var == 0)
+            showMessage(bosh + ' ');
+        else if (i % divisionsTal1Var == 0 && i % divisionsTal2Var === 0)
+            showMessage(bibo + ' ');
+        showMessage(i + ' ');
+    }
 }
-
 bishBosh();
 
 
 
-// --------  BISHBOSH ON CARD --------
 
-function showMessage(message) {
-    document.getElementById('test').innerHTML = message;
-    /* document.getElementById('test2').innerHTML = message; */
-}
+// ------ LÄS ANVÄNDARE ----------
 
-for (i = 0; i <= 100; i++) {
-    if (i % 3 === 0) 
-    showMessage(bish);
-    else if (i % 4 === 0) 
-    showMessage(bosh);
-    else if (i % 3 === 0 && i % 4 === 0) 
-    showMessage(bibo);
-    showMessage(i);
-}
+form['loopTal'].addEventListener('change', (e) => {
+    console.log(e.target(loopTalVar).value);
+})
+form['divisionsTal1'].addEventListener('change', (e) => {
+    console.log(e.target(divisionsTal1Var).value);
+})
+form['divisionsTal2'].addEventListener('change', (e) => {
+    console.log(e.target(divisionsTal2Var).value);
+})
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    output.innerHTML += `<p>${e.target[0].value}</p>`
+    e.target.reset(); //eller form.reset(); återställer fält 
+})
+
+
+
+
+
+// ------ END LÄS ANVÄNDARE ----------
+
+// form['loopTal'].addEventListener('focus', function(e){
+//     console.log('focus');
+//     this.classList.add('focus');
+// })
+
+// form['loopTal'].addEventListener('blur', function(e){
+//     console.log('blur');
+//     this.classList.add('blur');
+// })
+
+// form['loopTal'].addEventListener('change', (e) => {
+//     console.log(e.target.value);
+// })
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     output.innerHTML += `<p>${e.target[0].value}</p>`
+//     e.target.reset(); //eller form.reset(); återställer fält 
+// })
+
+
+
+//----- START Räknar klick & tar bort output ---
+// let counter = 1;
+
+// form.addEventListener('click', (e) => {
+//    output.innerHTML += `
+//    <div>
+//    <p>Hej: ${counter++} </p>
+//  </div>
+   
+//    `
+// })
+
+// output.addEventListener('click', (e) => {
+//      console.log(e.target);
+//      e.target.remove();
+// })
+//----- END Räknar klick & tar bort output ---
 
 
 // --------  BISHBOSH ON CONSOLE --------
 
-for (i = 0; i <= 100; i++) {
-    if (i % 3 == 0)
-        console.log(bish)
-    if (i % 4 == 0)
-        console.log(bosh)
-    if (i % 3 == 0 && i % 4 == 0)
-        console.log(bibo)
-    console.log(i)
-}
-
-
-// FUNGERAR
-
-// const test = document.querySelector('#test')
-// document.getElementById("test").innerHTML
-
-// let bish = 'Bish'
-// let bosh = 'Bosh'
-// let bibo = 'Bish-Bosh'
-
-// for (i = 1; i < 101; i++) {
-//     if (i % 3 == 0) 
-//     document.writeln(bish)
-//     if (i % 4 == 0) 
-//     document.writeln(bosh)
-//     if (i % 3 == 0 && i % 4 == 0) 
-//     document.writeln(bibo)
-//     document.writeln(i)
+// for (i = 0; i <= 100; i++) {
+//     if (i % 3 == 0)
+//         console.log(bish)
+//     if (i % 4 == 0)
+//         console.log(bosh)
+//     if (i % 3 == 0 && i % 4 == 0)
+//         console.log(bibo)
+//     console.log(i)
 // }
-
-
-const form = document.querySelector('#loginForm')
-
-form['firstName'].addEventListener('focus', function (e) {
-    console.log('focus');
-    this.classList.add('focus');
-})
-
