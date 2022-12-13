acd0424c69aa52d104c5ad65247b37ec8774f9b6
soroshdotnet <sorosh.farahmand@gmail.com>
2022-12-13 22:18:26 +0100

    ToDo list implamenterad!! :D

--- todo.js
+++ todo.js
@@ -1,50 +1,74 @@
 
 
-const form = document.querySelector('#todoForm')
-const todoOut = document.querySelector('#todoOut')
+const form = document.querySelector('#todoForm');
+const todoOut = document.querySelector('#todoOut');
+const square = document.querySelector('#square');
 
-let toDoVar = ' ';
+let toDoVar;
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    todoOut.innerHTML = '';
+    // todoOut.innerHTML = '';
 
     let toDoVar = document.getElementById("todoNote").value
 
     let res = []; 
-    res.push(toDoVar + ' ');
+    res.push(toDoVar);
     todoOut.innerHTML += res.map(n => `<span>${n}</span>`);
 
     e.target.reset();
 });
 
+
 todoOut.addEventListener('click', (e) => {
+    console.log(e.target);
+    e.target.style.background = `rgb(175, 164, 8)`
+    e.target.style.textDecoration = `line-through`
+});
+
+todoOut.addEventListener('dblclick', (e) => {
+    console.log(e.target);
+    e.target.style.background = ``
+    e.target.style.textDecoration = ``
+});
+
+//--------- FUNGERAR TAR BORT OUTPUT -------------
+todoOut.addEventListener('contextmenu', (e) => {
     console.log(e.target);
     e.target.remove();
 });
 
 
+// ------------------square----------------------
+// square.addEventListener('mousemove', e => {
+//     // console.log(e);
+//     // console.log(e.offsetX);
+//      square.style.background = `rgb(${e.offsetX}, ${e.offsetY}, 0)`
+// })
 
 
 
-// form['loopTal'].addEventListener('focus', function(e){
+
+// form['todoNote'].addEventListener('focus', function(e){
 //     console.log('focus');
 //     this.classList.add('focus');
 // })
 
-// form['loopTal'].addEventListener('blur', function(e){
+// form['todoNote'].addEventListener('blur', function(e){
 //     console.log('blur');
 //     this.classList.add('blur');
 // })
 
-// form['loopTal'].addEventListener('change', (e) => {
+
+
+// form['todoNote'].addEventListener('change', (e) => {
 //     console.log(e.target.value);
 // })
 
 // form.addEventListener('submit', (e) => {
 //     e.preventDefault();
 
-//     output.innerHTML += `<p>${e.target[0].value}</p>`
+//     todoOut.innerHTML += `<p>${e.target[0].value}</p>`
 //     e.target.reset(); //eller form.reset(); återställer fält 
 // })
 
